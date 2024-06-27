93a320df4a4ef8a3159a712d55540dce2328acf9
Arthur Almeida <arthuraraujo07@hotmail.com>
2024-06-27 14:40:20 -0300

    commit estilização e página home

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ with open('model.pkl', 'rb') as f:
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+@app.route('/search')
+def index():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
